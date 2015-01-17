84fdc0bafe0048bbc1f55ca9c3479339d9f1ed72
anlhord <anlhord@gmail.com>
2015-01-17 17:53:03 +0100

    Before the shortening of unneeded void chains

--- example_test.go
+++ example_test.go
@@ -77,6 +77,7 @@ func TestCustom0(t *testing.T) {
 }
 
 */
+
 func Append(src [2][][]byte, a ...byte) [2][][]byte {
 	return AppendS(src, a)
 }
@@ -99,8 +100,15 @@ func Collapse(dst *[]byte, src [2][][]byte) {
 		}
 	}
 }
+func mkshort(i int) int {
+	if i > 0 {
+		return -1
+	}
+	return 0
+}
 func Slice(dst *[2][][]byte, src [2][][]byte, i, p, j, q int) {
 	if i < 0 {
+		fmt.Println("aa")
 		n := len(src[1])
 		(*dst)[1] = append((*dst)[1], src[1][j-1+n:i+n]...)
 		l := len((*dst)[1]) - 1
@@ -108,12 +116,14 @@ func Slice(dst *[2][][]byte, src [2][][]byte, i, p, j, q int) {
 		(*dst)[1][l] = (*dst)[1][l][:q]
 		(*dst)[1][0] = (*dst)[1][0][p:]
 	} else if j >= 0 {
+		fmt.Println("bb")
 		(*dst)[0] = append((*dst)[0], src[0][i:j+1]...)
 		(*dst)[1] = [][]byte{}
 		l := len((*dst)[0]) - 1
 		(*dst)[0][l] = (*dst)[0][l][:q]
 		(*dst)[0][0] = (*dst)[0][0][p:]
 	} else {
+		fmt.Println("cc")
 		(*dst)[0] = append((*dst)[0], src[0][i:]...)
 		(*dst)[0][0] = (*dst)[0][0][p:]
 		(*dst)[1] = append((*dst)[1], src[1][:j-1+len(src[1])]...)
@@ -149,29 +159,64 @@ func TestCustom0(t *testing.T) {
 }
 
 func TestSlice0(t *testing.T) {
-	var list, list2, list3, list4 [2][][]byte
+	var list, list1, list2, list3, list01, list02, list03 [2][][]byte
 
-	list[0] = [][]byte{{1, 2, 3}, {4, 5, 6, 7, 8}, {9, 10, 11}, {12, 13}, {14, 15}}
-	list[1] = [][]byte{{16, 17}, {18, 19}, {20, 21, 22}, {23, 24}, {25, 26, 27}}
+	list = [2][][]byte{{{1, 2, 3}, {4, 5, 6, 7, 8}, {9, 10, 11}, {12, 13}, {14, 15}},
+		{{16, 17}, {18, 19}, {20, 21, 22}, {23, 24}, {25, 26, 27}}}
+
 	fmt.Println(list)
+	origlist := fmt.Sprintln(list)
+	c1 := "[[[7 8] [9 10 11] [12 13] [14 15]] [[16 17] [18]]]\n"
+	c2 := "[[[7 8] [9 10 11] [12]] []]\n"
+	c3 := "[[] [[19] [20 21]]]\n"
+
+	Slice(&list1, list, 1, 3, -2, 1)
+	if fmt.Sprintln(list1) != c1 {
+		t.Errorf("Bad list - output 1")
+	}
+	if fmt.Sprintln(list) != origlist {
+		t.Errorf("Damaged list - side effect 1")
+	}
+
+	Slice(&list2, list, 1, 3, 3, 1)
+	if fmt.Sprintln(list2) != c2 {
+		t.Errorf("Bad list - output 2")
+	}
+	if fmt.Sprintln(list) != origlist {
+		t.Errorf("Damaged list - side effect 2")
+	}
+
+	Slice(&list3, list, -2, 1, -3, 2)
+	if fmt.Sprintln(list3) != c3 {
+		t.Errorf("Bad list - output 3")
+	}
+	if fmt.Sprintln(list) != origlist {
+		t.Errorf("Damaged list - side effect 3")
+	}
+
+	fmt.Println("-----------------------------------------------")
+
+	Slice(&list01, list, 1, 0, -2, 0)
+	fmt.Println(list01)
+	if fmt.Sprintln(list) != origlist {
+		t.Errorf("Damaged list - side effect")
+	}
 
 	fmt.Println("----------------")
 
-	Slice(&list2, list, 1, 3, -2, 1)
-	fmt.Println(list2)
-	fmt.Println(list)
+	Slice(&list02, list, 1, 0, 3, 0)
+	fmt.Println(list02)
+	if fmt.Sprintln(list) != origlist {
+		t.Errorf("Damaged list - side effect")
+	}
 
 	fmt.Println("----------------")
 
-	Slice(&list3, list, 1, 3, 3, 1)
-	fmt.Println(list3)
-	fmt.Println(list)
-
-	fmt.Println("----------------")
-
-	Slice(&list4, list, -2, 1, -3, 2)
-	fmt.Println(list4)
-	fmt.Println(list)
+	Slice(&list03, list, -2, 0, -3, 0)
+	fmt.Println(list03)
+	if fmt.Sprintln(list) != origlist {
+		t.Errorf("Damaged list - side effect")
+	}
 }
 
 /*
